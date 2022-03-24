625e4889d3c4d8ff70947302fe834814e03a2c42
Justin Chen <ch3njus@gmail.com>
2022-03-24 14:07:31 -0400

    Get price for bytes

--- controllers/price.js
+++ controllers/price.js
@@ -1,10 +1,6 @@
 const axios = require('axios');
 
-exports.price = (req, res) => {
-	axios.get('https://api.coingecko.com/api/v3/simple/price?ids=arweave&vs_currencies=usd').then(result => {
-		res.json({
-			data: result.data.arweave.usd,
-			denomination: 'usd'
-		})
-	})
+exports.uploadEstimate = (req, res) => {
+	const bytes = req.originalUrl.split('/')[-1]
+	axios.get(`https://arweave.net/price/${bytes}`)
 };

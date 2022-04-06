ce044d3dfbb396341e8efc35ca206f5952d94f57
Justin Chen <ch3njus@gmail.com>
2022-04-06 13:16:21 -0400

    Fixed /price

--- controllers/price.js
+++ controllers/price.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 exports.uploadEstimate = (req, res) => {
+	console.log('/price')
 	const bytes = req.originalUrl.split('/')[-1]
 	axios.get(`https://arweave.net/price/${bytes}`)
 };

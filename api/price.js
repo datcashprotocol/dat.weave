bc7b280b231f8218587af0e33a1a323019ac94ae
Justin Chen <ch3njus@gmail.com>
2022-04-08 13:07:24 -0400

    Fixed price api

--- api/price.js
+++ api/price.js
@@ -2,6 +2,6 @@ const express = require('express')
 router = express.Router()
 api = require('../controllers/price.js')
 
-router.get('/', api.uploadEstimate)
+router.get('/*', api.uploadEstimate)
 
 module.exports = router
--- controllers/price.js
+++ controllers/price.js
@@ -2,6 +2,10 @@ const axios = require('axios');
 
 exports.uploadEstimate = (req, res) => {
 	console.log('/price')
-	const bytes = req.originalUrl.split('/')[-1]
+	const bytes = req.originalUrl.split('/').at(-1)
+	
 	axios.get(`https://arweave.net/price/${bytes}`)
+	.then((price) => {
+		res.json({ data: price.data })
+	})
 };

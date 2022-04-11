dd0148f2b872b1c67bd508700aba9179d094f801
Justin Chen <ch3njus@gmail.com>
2022-04-11 10:24:37 -0400

    Condition to always send back a price so can use locally

--- controllers/price.js
+++ controllers/price.js
@@ -5,7 +5,12 @@ exports.uploadEstimate = (req, res) => {
 	const bytes = req.originalUrl.split('/').at(-1)
 	
 	axios.get(`https://arweave.net/price/${bytes}`)
+	.catch((err) => {
+		res.json({ data: 1 }) // if cannont reach, send back resp anyways. e.g. dev'ing locally.
+	})
 	.then((price) => {
-		res.json({ data: price.data })
+		if(price.hasOwnProperty('data')) {
+			res.json({ data: price.data })
+		}
 	})
 };

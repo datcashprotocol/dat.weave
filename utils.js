2159c6fe0e42af3147cd7a1d1f30f7e4a6a267bf
Justin Chen <ch3njus@gmail.com>
2022-04-17 17:36:40 -0400

    Saving progress on clearing mongo

--- utils.js
+++ utils.js
@@ -3,7 +3,28 @@ const mongo = require('mongodb')
 exports.clear_mongo = () => {
 	const MongoClient = mongo.MongoClient;
 	MongoClient.connect('mongodb://localhost:27017/', function(err, db) {
+		const datweave = db.db('datweave')
 		if (err) throw err;
-		db.getCollectionNames().forEach(function(x) {db[x].drop()})
+
+		datweave.dropCollection('wallets', (err, ok) => {
+			if(err) {
+				throw err;
+			}
+
+			if(ok) {
+				console.log('cleared wallets')
+			}
+		})
+		datweave.dropCollection('transactions', (err, ok) => {
+			if(err) {
+				throw err;
+			}
+
+			if(ok) {
+				console.log('cleared transactions')
+			}
+
+			db.close()
+		})
 	});	
 };

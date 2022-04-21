4e69955c024dd42588ca8b66896f09673108576d
Justin Chen <ch3njus@gmail.com>
2022-04-21 11:28:45 -0400

    Added comment for /price and increased timeout

--- controllers/price.js
+++ controllers/price.js
@@ -4,7 +4,10 @@ exports.uploadEstimate = (req, res) => {
 	const bytes = req.originalUrl.split('/').at(-1) // TODO: make into a :bytes param in api/price.js
 	console.log(`GET /price/${bytes}`)
 	
-	axios.get(`https://arweave.net/price/${bytes}`, { timeout: 10000 })
+	// Note: If this Jest test does not pass, increase the timeout
+	// The default setting is 5000ms, but this didn't seem long enough during
+	// most tests even though this URL works in the browser.
+	axios.get(`https://arweave.net/price/${bytes}`, { timeout: 20000 })
 	.catch((err) => {
 		// if cannont reach, send back resp anyways. e.g. dev'ing locally.
 		res.status(404).json({ data: -1 })
